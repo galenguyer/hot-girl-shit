6c4f80356eb60bd8a15461bd22e6bfa4639a0cf4
Galen Guyer <galen@galenguyer.com>
2021-05-28 17:07:13 -0700

    Add vnet

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ resource "azurerm_resource_group" "azure_rg" {
   name = var.rg_name
   location = var.rg_region
 }
+
+resource "azurerm_virtual_network" "vnet" {
+ name                = "${var.rg_name}-vnet"
+ address_space       = ["10.0.0.0/16"]
+ location            = var.rg_region
+ resource_group_name = azurerm_resource_group.azure_rg.name
+}
