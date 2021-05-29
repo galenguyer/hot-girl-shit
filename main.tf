919066ff270ee654af442ce25094315b6cb23e8f
Galen Guyer <galen@galenguyer.com>
2021-05-28 17:03:27 -0700

    Add resource group with options

--- main.tf
+++ main.tf
@@ -11,3 +11,8 @@ provider "azurerm" {
   subscription_id = var.azure_subscription_id
   features {}
 }
+
+resource "azurerm_resource_group" "azure_rg" {
+  name = var.rg_name
+  location = var.rg_region
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable azure_subscription_id {
   type = string
   default = "00000000-0000-0000-0000-000000000000"
 }
+
+variable rg_name {
+    type = string
+    default = "hot-girl-shit"
+}
+
+variable rg_region {
+  type = string
+  default = "eastus"
+}
