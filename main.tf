5b3681bdd45baa3a480c93a9a5fe3518d364e882
Galen Guyer <galen@galenguyer.com>
2021-05-28 17:13:57 -0700

    Add NSG

--- main.tf
+++ main.tf
@@ -30,3 +30,22 @@ resource "azurerm_subnet" "subnet" {
  virtual_network_name = azurerm_virtual_network.vnet.name
  address_prefixes     = ["10.0.0.0/16"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+    name                = "${var.rg_name}-nsg"
+    location            = var.rg_region
+    resource_group_name = azurerm_resource_group.rg.name
+
+    # normal stuff
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
